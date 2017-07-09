948df6a0e22bb187fc2da6330c3590554a74ee36
Ardit Mezini <mezini.ardit@gmail.com>
2017-07-09 11:40:16 +0200

    Created Context and Seed method

---
+++ MvcExamples/Context/MvcContext.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using MvcExamples.Models.Visitor;
+using System;
+using Newtonsoft.Json;
+
+namespace MvcExamples.Context
+{
+    public class MvcContext : DbContext
+    {
+        public MvcContext() : base("name=MvcContext")
+        {
+            Database.SetInitializer(new SeedDb());
+        }
+
+        public virtual DbSet<VisitorDto> Visitor { get; set; }
+    }
+
+    public class SeedDb : CreateDatabaseIfNotExists<MvcContext>
+    {
+        protected override void Seed(MvcContext context)
+        {
+            #region Vistor
+            var _jsonPath = AppDomain.CurrentDomain.BaseDirectory + "App_Data\\visitor-mock.json";
+            var visitorObject = System.IO.File.ReadAllText(_jsonPath);
+            var visitor = JsonConvert.DeserializeObject<List<VisitorDto>>(visitorObject);
+            context.Visitor.AddRange(visitor);
+            #endregion
+
+            base.Seed(context);
+        }
+    }
+}
--- MvcExamples/Controllers/VisitorController.cs
+++ MvcExamples/Controllers/VisitorController.cs
@@ -1,14 +1,22 @@
 ﻿using MvcExamples.Models.Visitor;
 using System.Linq;
 using System.Web.Mvc;
+using MvcExamples.Context;
 
 namespace MvcExamples.Controllers
 {
-    public class VisitorController : BaseVisitorController
+    public class VisitorController : Controller
     {
+        private readonly MvcContext _context;
+
+        public VisitorController()
+        {
+            _context = new MvcContext();
+        }
+
         public ActionResult Index(VisitorSearchForm searchModel)
         {
-            visitorList = visitorList
+            var visitorList = _context.Visitor
                 .Where(x => searchModel.FirstName == null || x.FirstName.Contains(searchModel.FirstName))
                 .Where(x => searchModel.LastName == null || x.LastName.Contains(searchModel.LastName))
                 .Where(x => searchModel.Email == null || x.Email.Contains(searchModel.Email))
@@ -27,7 +35,7 @@ namespace MvcExamples.Controllers
         {
             if (id != 0)
             {
-                var entitiy = visitorList.FirstOrDefault(x => x.Id == id);
+                var entitiy = _context.Visitor.FirstOrDefault(x => x.Id == id);
                 return View(entitiy);
             }
             return RedirectToAction("Index");
